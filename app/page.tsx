8927803ed445b7d049d7fe04fad3868ec9b60f37
smithms91 <smithms91@gmail.com>
2023-01-03 19:35:56 -0500

    Added basic styles and simple homepage layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function HomePage() {
-    return <div className="hey">hey man</div>
+    return (
+        <div>
+            <div className="center-box">
+                <h3>Enter a command:</h3>
+                <form action="">
+                    <input type="text" />
+                </form>
+            </div>
+            <h1>Shady.gg</h1>
+        </div>
+    )
 }
